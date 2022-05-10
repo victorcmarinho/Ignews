f1c165a29b04406ef408a57b36a91924c9e3bbec
Victor Marinho <victorcmarinho@gmail.com>
2022-05-10 08:04:28 -0300

    finalizando ignews

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,8 +2,8 @@ import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
   const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
-    req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
   });
 
   return prismic;
